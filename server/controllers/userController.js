a25bd0550534652b95f743e81c594447acc00fe1
seif <seifeddine.aaza@gmail.com>
2023-10-23 02:24:46 +0100

    update

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { allUsers};
--- server/models/User.js
+++ server/models/User.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username : {
-        type : 'string',
-        required : true
+        type : String,
+        required : true,
+        validate : /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/
     },
     email : {
-        type : 'string',
+        type : String,
         required : true,
         unique : true,
-        index: true
+        index: true,
+        validate : /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     },
     password : {
-        type : 'string',
-        required : true
+        type : String,
+        required : true,
+        validate : /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/
     }
 })
 
